937fbc5de3db86cfc4d32db3976aa3f1b81a56ab
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-04-21 12:08:00 +0100

    fix: Link missing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Card from '@/components/card/card';
 import Image from 'next/image';
 import { Playfair_Display } from 'next/font/google';
+import Link from 'next/link'
 const playfair = Playfair_Display({ subsets: ['latin'] });
 
 export default function Home() {
